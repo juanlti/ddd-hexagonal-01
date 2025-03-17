3eeffbcef10a6ea38eeb43027e9b829c2768b7c7
juanc <juancruzliberati@hotmail.com>
2025-03-17 17:34:17 -0300

    proyecto base clonar rama principal

---
+++ src/platform/purchase/infrastructure/controllers/ExampleGETController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Src\platform\purchase\infrastructure\controllers;
+
+use App\Http\Controllers\Controller;
+
+final class ExampleGETController extends Controller {
+
+ public function index() {
+ // TODO: DDD Controller content here
+     echo 'Example GET Controller index';
+ }
+}
